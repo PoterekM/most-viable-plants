fe811c9deb6116bb40b1022f3389c16b03678967
Michelle Poterek <poterekm@gmail.com>
2017-09-28 11:28:03 -0700

    utilize weather API that provides high and low for day but seems innacurate

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,15 +15,24 @@ export function fetchWeather(searchCity) {
   return function (dispatch) {
     dispatch(requestWeather(searchCity));
     console.log(searchCity + " line 17");
-    return fetch("http://api.openweathermap.org/data/2.5/forecast?q=Portland&units=imperial&APPID=a928e20f76a0b5e801f9d90feb578f6a", {
+    return fetch("http://api.openweathermap.org/data/2.5/forecast/daily?q=" + searchCity + ",US&units=imperial&APPID=a928e20f76a0b5e801f9d90feb578f6a", {
       headers : {
         'Accept': 'application/json'
       }}).then(
       response => response.json(),
       error => console.log("There has been an error", error),
     ).then(function(json) {
-      console.log("this is line 26"),
-      console.log(json.list[0].dt_txt);
+      console.log(json)
+      console.log("today high" + json.list[0].temp.max);
+      console.log("tomorrow low" + json.list[0].temp.min);
+      console.log("Friday high" + json.list[1].temp.max);
+      console.log("Friday low" + json.list[1].temp.min);
+
+
+      // console.log(json.list[0].dt_txt);
+      // console.log(json.list[0].main.temp);
+      // console.log(json.list[1].dt_txt);
+      // console.log(json.list[1].main.temp);
         if (json > 0) {
         const weather = json.cod
         console.log("HEY");
