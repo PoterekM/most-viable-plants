6a49dbb12a7c45ddddfd90fb53d84f60a53184df
Michelle Poterek <poterekm@gmail.com>
2017-10-03 08:41:36 -0700

    add form area content branching to hide default text upon page load

--- src/components/WeatherDisplay.jsx
+++ src/components/WeatherDisplay.jsx
@@ -4,22 +4,36 @@ import { connect } from "react-redux";
 
 
 const WeatherDisplay = ({ dispatch, weather }) => {
+  let formAreaContent;
   if ((weather.todayWeatherMax) > 90) {
     alert("yaowza! Ya gotta watter your plants!");
   } else if ((weather.todayWeatherMin) < 32) {
     alert("Booooo!!");
   }
 
-  return (
+  if ((weather.todayWeatherMax) === "unknown high!"){
+    console.log("yaaa");
+    formAreaContent =
+      <div>
+      </div>
+  } else {
+    formAreaContent =
     <div>
       <h1>The high temperature for {weather.searchCity.searchCity} today is {weather.todayWeatherMax} with a low of {weather.todayWeatherMin}</h1>
-    <hr/>
-  <h1>Tomorrow</h1>
-The high temperature for {weather.searchCity.searchCity} today is {weather.tomorrowWeatherMax} with a low of {weather.tomorrowWeatherMin}
+      <hr/>
+      <h1>Tomorrow</h1>
+      The high temperature for {weather.searchCity.searchCity} today is {weather.tomorrowWeatherMax} with a low of {weather.tomorrowWeatherMin}
 
       <hr/><hr/><hr/>
       <h1>{weather.weather}</h1>
       <h2>{weather.searchCity.searchCity}</h2>
+    </div>
+  }
+
+
+  return (
+    <div>
+      {formAreaContent}
     </div>
   )
 }
