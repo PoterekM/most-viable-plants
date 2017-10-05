f056cc6a44a3f61e17fac8f2340040f76f54b9c1
Michelle Poterek <poterekm@gmail.com>
2017-10-05 13:10:13 -0700

    console log date from button click- need to change into having another action

--- src/components/Plant.jsx
+++ src/components/Plant.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Moment from "moment";
 
 function Plant(props){
+  console.log(props);
+  function handleTimeSince() {
+    let lastWatered = new Date();
+    console.log("hello");
+    console.log(lastWatered);
+  }
 
   return (
     <div>
       <h3>{props.plantName} - {props.plantType}</h3>
       <p><em>Water {props.waterSchedule}</em></p>
-      <button>Water!</button>
+    <button onClick={() => {handleTimeSince(props.lastWatered)}}>Water!</button>
       <p><em>Last watered {props.lastWatered}</em></p>
       <hr/>
     </div>
@@ -19,6 +26,7 @@ Plant.propTypes = {
   plantType: PropTypes.string,
   waterSchedule: PropTypes.string,
   lastWatered: PropTypes.string,
+
 };
 
 export default Plant;
